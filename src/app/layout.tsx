a2a94dd29e2c588014661fb2c5fc8ca4b817c6ca
Vieque <lier19vel@gmail.com>
2025-04-10 10:35:59 +0700

    #17 Main UI initial

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased  min-w-screen min-h-screen`}
       >
         <OceanBackground />
         <FCMToken />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
-"use client";
-import { Button } from "@/components/ui/button";
+'use client';
+import React from 'react';
+import { Button } from '@/components/ui/button';
+import { Bell } from 'lucide-react';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -7,31 +9,30 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Bell } from "lucide-react";
-import React from "react";
+} from "@/components/ui/dropdown-menu"
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
 
 const TopNavigation: React.FC = () => {
   return (
-    <div className="flex items-center justify-between px-10 py-2 backdrop-filter backdrop-blur-sm bg-white/30 border-b">
+    <div className="fixed top-0 w-screen flex items-center justify-between px-2 md:px-10 h-[60px] backdrop-filter backdrop-blur-sm bg-white/30 border-b">
       <div className="flex items-center space-x-4">
         <a href="/" className="text-lg font-semibold text-gray-800">
-          <img
-            src="/images/OrangeSEA Horizontal.png"
-            alt="Logo"
-            className="h-10 w-auto"
-          />
+          <img src="/images/OrangeSEA Horizontal.png" alt="Logo" className="h-6 md:h-10 w-auto" />
         </a>
+
       </div>
       <div className="flex items-center space-x-4">
-        <Button variant="outline" className="text-gray-800 hover:bg-gray-100">
-          NEED HELP
-        </Button>
+        <Button variant="outline" className="text-gray-800 hover:bg-gray-100">NEED HELP</Button>
         <Button variant="ghost" size="icon">
           <Bell className="text-gray-800" />
         </Button>
         <DropdownMenu>
-          <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" alt="avatar" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
           <DropdownMenuContent>
             <DropdownMenuLabel>My Account</DropdownMenuLabel>
             <DropdownMenuSeparator />
@@ -41,8 +42,9 @@ const TopNavigation: React.FC = () => {
             <DropdownMenuItem>Subscription</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
+
       </div>
     </div>
   );
-};
+}
 export default TopNavigation;
