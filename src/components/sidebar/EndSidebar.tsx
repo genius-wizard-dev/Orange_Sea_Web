f1a8dfcbb3adaef7f0eb5ea4d66b041d895b4bca
Vieque <lier19vel@gmail.com>
2025-04-13 15:32:19 +0700

    #45 Add fully basic responsive support

--- src/components/sidebar/EndSidebar.tsx
+++ src/components/sidebar/EndSidebar.tsx
@@ -1,9 +1,33 @@
+import { cn } from '@/lib/utils';
 import React from 'react';
+import { Button } from '../ui/button';
 
-const EndSidebar: React.FC = () => {
+interface EndSidebarProps {
+	children?: React.ReactNode,
+	className?: string,
+	hidden?: boolean
+	onClose?: () => void
+}
+
+const EndSidebar: React.FC<EndSidebarProps> = ({
+	children,
+	className = "",
+	hidden = false,
+	onClose,
+}) => {
 	return (
-		<div className="w-1/4 h-full bg-gray-200 p-4 overflow-y-auto overflow-x-hidden border-s backdrop-filter backdrop-blur-sm bg-white/30">
-			<h2>End Sidebar</h2>
+		<div className={cn( 
+			"w-1/4 h-full p-4 overflow-y-auto overflow-x-hidden border-s backdrop-filter backdrop-blur-sm bg-white/30 z-30",
+			className,
+			"overflow-x-hidden",
+			"fixed top-0 right-0 w-screen", // Responsive smaller screens
+			"lg:top-0 lg:right-0 lg:w-[400px]", // Larger screens
+			"xl:relative xl:w-[400px]", // Extra large screens
+			"transition-all duration-300 ease-in-out",
+			hidden ? "hidden" : "visible",
+		)}>
+			<Button variant="outline" className="mb-4" onClick={onClose}>Close</Button>
+
 		</div>
 	);
 };
--- src/components/sidebar/StartSidebar.tsx
+++ src/components/sidebar/StartSidebar.tsx
@@ -1,12 +1,26 @@
+import { cn } from '@/lib/utils';
 import React from 'react';
 
 interface StartSidebarProps {
-	children?: React.ReactNode;
+	children?: React.ReactNode,
+	className?: string,
+	hidden?: boolean
+	onClose?: () => void
 }
 
-const StartSidebar: React.FC<StartSidebarProps> = ({ children }) => {
+const StartSidebar: React.FC<StartSidebarProps> = ({ 
+	children,
+	className = "",
+	hidden = false,
+	onClose,
+ }) => {
 	return (
-		<div className="w-[500px] h-full bg-gray-200 p-4 overflow-y-auto overflow-x-hidden border-e backdrop-filter backdrop-blur-sm bg-white/30">
+		<div className={cn(
+			"h-full p-4 overflow-y-auto overflow-x-hidden border-e backdrop-filter backdrop-blur-sm bg-white/30",
+			"top-0 left-0 w-screen w-full", // Responsive smaller screens
+			"lg:top-0 lg:left-0 lg:w-[500px]", // Larger screens
+			"xl:w-[500px]", // Extra large screens
+		)}>
 			{children}
 		</div>
 	);
